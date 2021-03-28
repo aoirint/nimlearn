37cb8593f81b3fddbc5162adbe4b6d2fced1f9ec
aoirint <aoirint@gmail.com>
2021-03-28 15:10:22 +0900

    variable

---
+++ variable/reassign_let.nim
@@ -0,0 +1,6 @@
+# compile error
+# nimlearn/variable/reassign_let.nim(6, 1) Error: 'a' cannot be assigned to
+
+let a: int = 33
+
+a = 4 # compile error
---
+++ variable/variable.nim
@@ -0,0 +1,11 @@
+
+const a: int = 123
+let b: int = 555
+var c: int = 321
+
+c += a
+c += b
+
+echo(c)
+
+assert c == 999
